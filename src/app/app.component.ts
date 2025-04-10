375cbe47b5644dddcc795093b52413e9a8066e7c
Ricsius <hajnal.ricsi@gmail.com>
2025-04-10 19:32:56 +0200

    Add divState animation trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(300))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal'
   list: string[] = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item: string) {
@@ -13,5 +29,9 @@ export class AppComponent {
 
     onDelete(item: string) {
 
+    }
+
+    onAnimate() {
+      this.state = this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
     }
 }
