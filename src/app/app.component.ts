2128c14bd0d103fa2e4bc6fd426b5863df38cb82
Ricsius <hajnal.ricsi@gmail.com>
2025-04-10 20:28:17 +0200

    Add wild animation trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,38 @@ import { Component } from '@angular/core';
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(300))
-    ])
-  ]
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange'
+      }),
+      animate(1000, style({
+        borderRadius: '50px'
+      })),
+      animate(500)
+    ]),
+  ])
+]
 })
 export class AppComponent {
-  state = 'normal'
+  state = 'normal';
+  wild = 'normal';
   list: string[] = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item: string) {
@@ -33,5 +58,10 @@ export class AppComponent {
 
     onAnimate() {
       this.state = this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wild = this.wild === 'normal' ? this.wild = 'highlighted' : this.wild = 'normal';
+    }
+
+    onShrink() {
+      this.wild = 'shrunken';
     }
 }
